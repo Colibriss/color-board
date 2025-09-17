1482bec4ebaa217e00025f7c86a96b52af1cc2b1
Colibriss <119446471+Colibriss@users.noreply.github.com>
2025-09-17 13:27:39 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+const board = document.querySelector('#board');
+const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71'];
+const SQUARES_NUMBER = 500;
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+  const square = document.createElement('div');
+  square.classList.add('square');
+  square.addEventListener('mouseover', () => setColor(square));
+  square.addEventListener('mouseleave', () => deleteColor(square));
+  board.append(square);
+}
+
+function setColor(element) {
+  const color = getRandomColor();
+  element.style.background = color;
+  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+}
+
+function deleteColor(element) {
+  element.style.background = '';
+  element.style.boxShadow = `0 0 2px #000`;
+}
+
+function getRandomColor() {
+  const inx = Math.floor(Math.random() * colors.length);
+  return colors[inx];
+}
